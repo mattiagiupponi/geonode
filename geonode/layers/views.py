38db60a90b0d5dc5c66e867efdad50e706671544
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-03-24 16:54:18 +0100

    fix

--- geonode/layers/views.py
+++ geonode/layers/views.py
@@ -1424,8 +1424,8 @@ def gs_append_data_to_layer(layer):
         upload_session.processed = False
         upload_session.save()
         #import_session = gs_uploader.start_import(upload_session.id)
-
         z = gs_uploader.upload(file, import_id=upload_session.id)
+        z.commit()
 
     return
 @login_required
